f9771606f8f53c5d7635ce62f860736f790c7c92
Mohammad Sianaki <m.rsianaki@yahoo.com>
2019-11-08 18:18:22 +0330

    Add flow call adapter

---
+++ FlowRetrofitAdapter/src/main/java/me/sianaki/flowretrofitadapter/FlowCallAdapter.kt
@@ -0,0 +1,63 @@
+package me.sianaki.lib
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.suspendCancellableCoroutine
+import retrofit2.Call
+import retrofit2.CallAdapter
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.reflect.Type
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+class ResponseCallAdapter<T>(
+    private val responseType: Type
+) : CallAdapter<T, Flow<Response<T>>> {
+    override fun adapt(call: Call<T>): Flow<Response<T>> {
+        return flow {
+            emit(
+                suspendCancellableCoroutine { continuation ->
+                    call.enqueue(object : Callback<T> {
+                        override fun onFailure(call: Call<T>, t: Throwable) {
+                            continuation.resumeWithException(t)
+                        }
+
+                        override fun onResponse(call: Call<T>, response: Response<T>) {
+                            continuation.resume(response)
+                        }
+                    })
+                }
+            )
+        }
+    }
+
+    override fun responseType() = responseType
+}
+
+
+class BodyCallAdapter<T>(private val responseType: Type) : CallAdapter<T, Flow<T>> {
+    override fun adapt(call: Call<T>): Flow<T> {
+        return flow {
+            emit(
+                suspendCancellableCoroutine { continuation ->
+                    call.enqueue(object : Callback<T> {
+                        override fun onFailure(call: Call<T>, t: Throwable) {
+                            continuation.resumeWithException(t)
+                        }
+
+                        override fun onResponse(call: Call<T>, response: Response<T>) {
+                            try {
+                                continuation.resume(response.body()!!)
+                            } catch (e: Exception) {
+                                continuation.resumeWithException(e)
+                            }
+                        }
+                    })
+                }
+            )
+        }
+    }
+
+    override fun responseType() = responseType
+}
